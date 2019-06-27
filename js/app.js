e7a47fd37bc5475dc9c905bd95905462b5445cf4
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-06-27 12:41:20 +0300

    final

--- js/app.js
+++ js/app.js
@@ -19,3 +19,24 @@ $(".main").onepage_scroll({
 });
 /*  https://github.com/peachananr/onepage-scroll */
 
+
+
+
+
+// menu
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+    });
+    
+    $('.menu__links-item').on('click', function() {
+      // do something
+
+      $(this).closest('.menu')
+        .removeClass('menu_state_open');
+    });
+  });
+})(jQuery);
